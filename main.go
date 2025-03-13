385a9cca52293566158a793794ad9746db9efc0c
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2025-03-13 19:34:22 +0200

    chore: Initialize Go backend and project structure with Wails framework

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed frontend/dist
+var assets embed.FS
+
+func main() {
+	app := NewApp()
+
+	err := wails.Run(&options.App{
+		Title:  "Gitmoji for your commit messages",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		OnStartup:  app.startup,
+		OnShutdown: app.shutdown,
+		Bind: []interface{}{
+			app,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewApp() *App {
+	return &App{}
+}
+
+type App struct {
+	ctx context.Context
+}
+
+func (b *App) startup(ctx context.Context) {
+	b.ctx = ctx
+}
+
+func (b *App) shutdown(ctx context.Context) {}
